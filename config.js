0c9264db01453f8871ae559adcdc37d1a1eb744e
mseaward <m.seaward@pbsa.info>
2018-11-09 15:34:02 -0400

    Update connections.

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const config = {
   environments: {
-    baxter: {
+    beatrice: {
       assetId: '1.3.1',
       apiEndpoints: [
         'wss://pta.blockveritas.co:8089', // taconator-witness
         'wss://bnode2.peerblock.trade', // bitcoinsig-test
-        'wss://ppytest.proxyhosts.info/wss', // tf-witness
         'wss://api.ppytest.nuevax.com', // nuevax-test
-        'wss://api.ppytest.blckchnd.com', // blckchnd-testnet
-        'wss://139.ip-54-38-243.eu', // melea-witness
-        'wss://api.test.ppy.us.altcap.io', // wwcd
-        'wss://baxter.ppy.steemul.ru:8080' // xtar-testnet
+        'wss://api.ppy-beatrice.blckchnd.com', // blckchnd-testnet
+        'wss://ip100.ip-54-37-165.eu/', // melea-witness
+        'wss://api.test.ppy.us.altcap.io', // winner.winner.chicken.dinner
+        'wss://peerplaysblockchain.net/testnet/api' // houdini-witness
       ],
-      faucetUrls: ['http://faucet.bookiepro.fun:5000'],
-      prefix: 'PPYTEST',
+      faucetUrls: ['https://pbsa-beatrice-faucet.blockchainprojectsbv.com'],
+      prefix: 'TEST',
       accounts: {
         broadcasts: {
           name: 'pbsa-broadcasts',
@@ -49,7 +48,7 @@ const config = {
         'ws://ec2-35-182-93-168.ca-central-1.compute.amazonaws.com:8090'
       ],
       faucetUrls: ['http://ec2-35-182-93-168.ca-central-1.compute.amazonaws.com:5000'],
-      prefix: 'PPY',
+      prefix: 'TEST',
       accounts: {
         broadcasts: {
           name: 'pbsa-broadcasts',
@@ -64,12 +63,12 @@ const config = {
     elizabeth: {
       assetId: '1.3.1',
       apiEndpoints: [
-        'ws://ec2-35-182-42-231.ca-central-1.compute.amazonaws.com:8090', // Canada
+        'ws://ec2-35-183-70-167.ca-central-1.compute.amazonaws.com:8090', // Canada
         'ws://ec2-18-195-242-233.eu-central-1.compute.amazonaws.com:8090', // Frankfurt
         'ws://ec2-54-252-182-8.ap-southeast-2.compute.amazonaws.com:8090' // Sydney
       ],
-      faucetUrls: ['http://ec2-35-182-42-231.ca-central-1.compute.amazonaws.com:5000'],
-      prefix: 'PPY',
+      faucetUrls: ['http://ec2-35-183-70-167.ca-central-1.compute.amazonaws.com:5000'],
+      prefix: 'TEST',
       accounts: {
         broadcasts: {
           name: 'pbsa-broadcasts',
@@ -84,17 +83,19 @@ const config = {
   }
 };
 
-let current = 'dick'; // Set the current to dick by default.
+let current = 'elizabeth'; // Set the current to elizabeth by default.
 
 // Check to make sure a valid target is specified.
 if (process.env && process.env.TARGET && config.environments[process.env.TARGET]) {
   current = process.env.TARGET;
   console.log(`Environment defined as ${current}`);
 } else if (!process.env.TARGET) {
-  console.warn('Environment was not defined, using default. (dick)');
+  console.warn(`Environment was not defined, using default. (${current})`);
 } else if (!config.environments[process.env.TARGET]) {
-  console.warn('Environment supplied is invalid, using default. (dick)');
+  console.warn(`Environment supplied is invalid, using default. (${current})`);
 }
 
 // Export the current config with the additional name property.
-module.exports = Object.assign(config.environments[current], {name: current});
+module.exports = Object.assign(config.environments[current], {
+  name: current
+});
